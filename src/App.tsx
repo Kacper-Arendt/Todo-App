86044679a2f3e33533dc160689faa208306e8c69
kacper <kacykvaa@gmail.com>
2021-05-27 18:45:13 +0200

    style: changed background name

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import {TodoList} from "./components/TodoList";
 import {AddTodo} from "./components/AddTodo";
 import {createGlobalStyle} from "styled-components";
-import background from './assets/sheep-6262928_1920.jpg'
+import Background from './assets/Background.jpg'
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -22,7 +22,7 @@ const GlobalStyle = createGlobalStyle`
     bottom: 0;
     right: 0;
     left: 0;
-    background-image: url(${background});
+    background-image: url(${Background});
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
